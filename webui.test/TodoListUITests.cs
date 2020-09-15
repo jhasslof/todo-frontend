23eb07119d18ad2e88e7ee2ec7b5174561d90d31
Janne Hasslöf <janne@ketchdigital.com>
2020-09-15 19:51:28 +0200

    ui tests for EditName and AddItem

--- webui.test/TodoListUITests.cs
+++ webui.test/TodoListUITests.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -75,5 +76,73 @@ namespace webui.test
             Assert.Equal(idToFind, rowColumnsToFind.ElementAt(0).Text);
             Assert.Equal(nameToFind, rowColumnsToFind.ElementAt(1).Text);
         }
+
+        [Fact]
+        public void TodoList_EditName_Succeed()
+        {
+            long idToEdit = 1;
+            Browser.Navigate().GoToUrl(Server.RootUri);
+            OpenEditForTodoItemRow(idToEdit);
+
+            var tbName = Browser.FindElementById("Name");
+            Assert.Equal("Buy new phone", tbName.GetAttribute("value"));
+
+            tbName.Clear();
+            tbName.SendKeys("Buy the new iPhone 12");
+            Browser.FindElementByClassName("btn").Click();
+
+            ReadOnlyCollection<IWebElement> updatedTodoItemRow = FindTodoItemRow(idToEdit);
+            int indexForNameColumn = 1;
+            Assert.Equal("Buy the new iPhone 12", updatedTodoItemRow[indexForNameColumn].Text);
+        }
+
+        [Fact]
+        public void TodoList_AddItem_Succeed()
+        {
+            string NameToAdd = "Go to the beach";
+            Browser.Navigate().GoToUrl(Server.RootUri);
+            OpenCreateView();
+
+            var tbName = Browser.FindElementById("Name");
+            Assert.Equal("", tbName.GetAttribute("value"));
+
+            tbName.SendKeys(NameToAdd);
+            Browser.FindElementByClassName("btn").Click();
+
+            ReadOnlyCollection<IWebElement> updatedTodoItemRow = FindTodoItemRow(5);
+            int indexForNameColumn = 1;
+            Assert.Equal(NameToAdd, updatedTodoItemRow[indexForNameColumn].Text);
+        }
+
+        private void OpenCreateView()
+        {
+            var newItem = Browser.FindElement(By.XPath("/html/body/div/main/p/a"));
+            Assert.Equal("Create New", newItem.Text);
+            newItem.Click();
+        }
+
+        private void OpenEditForTodoItemRow(long id)
+        {
+            ReadOnlyCollection<IWebElement> todoItemRow = FindTodoItemRow(id);
+            var indexForEditColumn = todoItemRow.Count - 1; //Index of last column
+            var editColumn = todoItemRow[indexForEditColumn];
+            var aTags = editColumn.FindElements(By.TagName("a"));
+            aTags.Single(t => t.Text == "Edit").Click();
+        }
+
+        private ReadOnlyCollection<IWebElement> FindTodoItemRow(long id)
+        {
+            var rows = Browser.FindElementsByXPath("//table/tbody/tr");
+            foreach (var row in rows)
+            {
+                var columns = row.FindElements(By.TagName("td"));
+                if (columns[0].Text == $"{id}")
+                {
+                    return columns;
+                }
+            }
+            return null;
+        }
+
     }
 }
--- webui.test/UITestHelpers.cs
+++ webui.test/UITestHelpers.cs
@@ -13,7 +13,7 @@ namespace webui.test
 
     class UITestHelpers
     {
-        protected void WaitForElementByClassNameExists(ChromeDriver driver, string className)
+        public static void WaitForElementByClassNameExists(ChromeDriver driver, string className)
         {
             Thread.Sleep(1000);
             var timeout = 10000; // in milliseconds
@@ -21,7 +21,7 @@ namespace webui.test
             wait.Until(ExpectedConditions.ElementExists(By.ClassName(className)));
         }
 
-        protected void WaitForElementByIdExists(ChromeDriver driver, string id)
+        public static void WaitForElementByIdExists(ChromeDriver driver, string id)
         {
             Thread.Sleep(1000);
             var timeout = 10000; // in milliseconds
@@ -29,7 +29,7 @@ namespace webui.test
             wait.Until(ExpectedConditions.ElementExists(By.Id(id)));
         }
 
-        protected string GetRandomString(int length = 5)
+        public static string GetRandomString(int length = 5)
         {
             if (length > 31)
             {
@@ -38,7 +38,7 @@ namespace webui.test
             return Guid.NewGuid().ToString().Substring(1, length);
         }
 
-        //protected void LoginUser(ChromeDriver driver, Uri loginPage)
+        //public static void LoginUser(ChromeDriver driver, Uri loginPage)
         //{
         //    driver.Navigate().GoToUrl(loginPage);
         //    //WaitForElementByIdExists(driver, "UserName");
@@ -48,7 +48,7 @@ namespace webui.test
         //    driver.FindElementById("btnLogin").Click();
         //}
 
-        protected void NavigateTo(ChromeDriver driver, string menuItemName)
+        public static void NavigateTo(ChromeDriver driver, string menuItemName)
         {
             var menu = driver.FindElementByClassName("main-menu");
             var menuItems = menu.FindElements(By.ClassName("navigationElement"));
